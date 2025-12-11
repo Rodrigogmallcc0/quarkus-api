e04f6d135af223a02377f1393e5b591d58af4e7a
rodrigogmallcco <rodrigogmallcco@outlook.com>
2025-12-10 22:28:24 -0500

    update properties and Credential to use mysql

---
+++ src/main/java/org/administration/quarkus/env/Credential.java
@@ -0,0 +1,7 @@
+package org.administration.quarkus.env;
+
+public class Credential {
+    public static final String DB_URL = System.getenv("DB_URL");
+    public static final String DB_USERNAME = System.getenv("DB_USERNAME");
+    public static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
+}
